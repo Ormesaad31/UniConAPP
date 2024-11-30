f5df3ba45bdfe93a00a0e81f0be0a4e33a1055bc
Ormesaad31 <138371727+Ormesaad31@users.noreply.github.com>
2024-11-30 21:25:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
-
+import fetch from "node-fetch";
 const app = express();
 
 const PORT = 8080;
